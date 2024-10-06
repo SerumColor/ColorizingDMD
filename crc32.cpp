3a70511194c9f1b75377ceb34fef9a0345cb9802
David <okidoaki@hotmail.fr>
2024-10-06 14:39:52 +0200

    V3.6.1

--- crc32.cpp
+++ crc32.cpp
@@ -2,7 +2,7 @@
 
 uint32_t crc32_table[256];
 
-void build_crc32_table(void) 
+void build_crc32_table(void) // initiating the CRC table, must be called at startup
 {
     for (uint32_t i = 0; i < 256; i++)
     {
@@ -19,7 +19,7 @@ void build_crc32_table(void)
     }
 }
 
-uint32_t crc32_fast(const UINT8* s, size_t n, BOOL ShapeMode) 
+uint32_t crc32_fast(const UINT8* s, size_t n, BOOL ShapeMode) // computing a buffer CRC32, "build_crc32_table()" must have been called before the first use
 {
     uint32_t crc = 0xFFFFFFFF;
     for (size_t i = 0; i < n; i++)
@@ -31,7 +31,7 @@ uint32_t crc32_fast(const UINT8* s, size_t n, BOOL ShapeMode)
     return ~crc;
 }
 
-uint32_t crc32_fast_step(const UINT8* s, const UINT step, size_t n, BOOL ShapeMode) 
+uint32_t crc32_fast_step(const UINT8* s, const UINT step, size_t n, BOOL ShapeMode) // computing a buffer CRC32, "build_crc32_table()" must have been called before the first use
 {
     uint32_t crc = 0xFFFFFFFF;
     UINT j = 0;
@@ -45,8 +45,8 @@ uint32_t crc32_fast_step(const UINT8* s, const UINT step, size_t n, BOOL ShapeMo
     return ~crc;
 }
 
-uint32_t crc32_fast_count(const UINT8* s, size_t n, BOOL ShapeMode, UINT8* pncols) 
-
+uint32_t crc32_fast_count(const UINT8* s, size_t n, BOOL ShapeMode, UINT8* pncols) // computing a buffer CRC32, "build_crc32_table()" must have been called before the first use
+// this version counts the number of different values found in the buffer
 {
     *pncols = 0;
     bool usedcolors[256];
@@ -66,8 +66,8 @@ uint32_t crc32_fast_count(const UINT8* s, size_t n, BOOL ShapeMode, UINT8* pncol
     return ~crc;
 }
 
-uint32_t crc32_fast_mask_shape(const UINT8* source, const UINT8* mask, size_t n, BOOL ShapeMode) 
-
+uint32_t crc32_fast_mask_shape(const UINT8* source, const UINT8* mask, size_t n, BOOL ShapeMode) // computing a buffer CRC32 on the non-masked area, "build_crc32_table()" must have been called before the first use
+// take into account if we are in shape mode
 {
     uint32_t crc = 0xFFFFFFFF;
     for (size_t i = 0; i < n; i++)
@@ -82,7 +82,7 @@ uint32_t crc32_fast_mask_shape(const UINT8* source, const UINT8* mask, size_t n,
     return ~crc;
 }
 
-uint32_t crc32_fast_mask(const UINT8* source, const UINT8* mask, size_t n) 
+uint32_t crc32_fast_mask(const UINT8* source, const UINT8* mask, size_t n) // computing a buffer CRC32 on the masked area, "build_crc32_table()" must have been called before the first use
 {
     uint32_t crc = 0xFFFFFFFF;
     for (size_t i = 0; i < n; i++)
